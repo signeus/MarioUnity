6e94baffd5e966f20f8ec281f66f91aa1104965f
signeus <coolasduck09@gmail.com>
2016-12-30 21:02:50 +0100

    Initial project

---
+++ Assets/scripts/CharacterMove.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMove : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    
+    //AXIS
+    bool right = false;
+    bool left = false;
+    bool up = false;
+    bool down = false;
+
+	// Use this for initialization
+	void Start () {
+	    //TODO Get Animator component when is created
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        //TODO Animation with the speed and direction
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if(move > 0 && !right)
+        {
+            Flip();
+        }
+        else
+        {
+            if(move < 0 && right)
+            {
+                Flip();
+            }
+        }
+    }
+
+    void Flip()
+    {
+        right = !right;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
